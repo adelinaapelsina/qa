a76c174b2588e6b030cb0afe477ccf225f50f201
Adelina <adelinagimaevaaa@gmail.com>
2026-06-02 21:14:14 +0300

    Lesson 4_2

---
+++ src/Lesson4_2/Circle.java
@@ -0,0 +1,29 @@
+package Lesson4_2;
+
+public class Circle implements Shape {
+    private double radius;
+    private String fillColour;
+    private String borderColour;
+
+    public Circle(double radius, String fillColour, String borderColour) {
+        this.radius = radius;
+        this.fillColour = fillColour;
+        this.borderColour = borderColour;
+    }
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+    @Override
+    public String getFillColour() {
+        return fillColour;
+    }
+    @Override
+    public String getBorderColour() {
+        return borderColour;
+    }
+}
